5ebbb621c5275643e67b967c3485533f0a58c7eb
Daniel-Marian Danila <daniladanielmarian@gmail.com>
2023-04-16 15:55:59 +0300

    Modified DataTableList component by adding the following props: data, columns, customInitialFilters so it can be used by multiple datasets (and also adapted the component code for this new features). Now router, pageRoute and currentPage (all of them use useRouter() from nextjs) are now global variables

--- front-end/src/components/SidebarElement.js
+++ front-end/src/components/SidebarElement.js
@@ -1,12 +1,9 @@
 import styles from "../styles/SidebarElement.module.css";
-import { useRouter } from "next/router";
+
 export default function SidebarElement({ name, icon }) {
-  const router = useRouter();
-  const pageRoute = router.pathname;
-  const currentPage = pageRoute.slice(1, pageRoute.length).toLocaleLowerCase();
   const isCurrentPage =
-    currentPage === name.toLocaleLowerCase() ||
-    (pageRoute === "/" && name == "Home");
+    global.currentPage === name.toLocaleLowerCase() ||
+    (global.pageRoute === "/" && name == "Home");
 
   return (
     <div
--- front-end/src/pages/_app.js
+++ front-end/src/pages/_app.js
@@ -4,8 +4,13 @@ import "primereact/resources/primereact.min.css";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
+  global.router = useRouter();
+  global.pageRoute = router.pathname;
+  global.currentPage = pageRoute.slice(1, pageRoute.length).toLocaleLowerCase();
+
   return (
     <>
       <Navbar />
